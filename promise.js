ddc2174c5f7f84fdddd52d841ee19340fb3c955f
Faina Harmash <faya.garmash.work@gmail.com>
2020-03-08 21:48:34 +0200

    added promise, comments belong to posts with class- display:none

--- promise.js
+++ promise.js
@@ -11,20 +11,75 @@ function xhr(method = "", url, body = null) {
   });
 }
 
-function usersLoad(response = []) {
-  response.forEach(element => {
+const usersLoad = function(users) {
+  users.forEach(element => {
     let p = document.createElement("p");
+    p.setAttribute("class", "position");
     let ul = document.createElement("ul");
-    let li = document.createElement("li");
-    
+    let ol = document.createElement("ol");
+    let blockquote = document.createElement("blockquote");
+
     p.setAttribute("data-id", element.id);
-    p.innerText = `${element.title} / ${element.body}`;
-    li.innerText = `${element.title}`;
+    p.innerText = `Name: ${element.name} / Email: ${element.email} / Company: ${element.company.name}`;
+
     p.append(ul);
+    p.append(ol);
+    p.append(blockquote);
+
     document.body.append(p);
   });
-}
-xhr("GET", "https://jsonplaceholder.typicode.com/posts").then(res=> postsLoad(res))
-.then(()=> {
-    xhr("GET","https://jsonplaceholder.typicode.com/comments")
-})
+};
+
+const albumsLoad = function(albums) {
+  albums.forEach(album => {
+    let user = document.querySelector(`[data-id="${album.userId}"]`);
+    let ul = user.querySelector("ul");
+    let li = document.createElement("li");
+    li.innerText = "Album title: " + album.title;
+
+    ul.append(li);
+  });
+};
+
+const postsLoad = function(posts) {
+  posts.forEach(post => {
+    let user = document.querySelector(`[data-id="${post.userId}"]`);
+    let ol = user.querySelector("ol");
+    let li = document.createElement("li");
+
+    li.innerText = "Post title: " + post.title;
+    li.setAttribute("post-data-id", post.id);
+    ol.append(li);
+    let olComment = document.createElement("ol");
+    olComment.setAttribute("class", "text_format");
+    li.append(olComment);
+  });
+};
+
+const commentsLoad = function(comments) {
+  comments.forEach(comment => {
+    let post = document.querySelector(`[post-data-id="${comment.postId}"]`);
+
+    let ol = post.querySelector("ol");
+    let li = document.createElement("li");
+    li.innerText = "Comment: " + comment.body;
+    ol.append(li);
+  });
+};
+
+xhr("GET", "https://jsonplaceholder.typicode.com/users")
+  .then(function(users) {
+    usersLoad(users);
+    return xhr("GET", "https://jsonplaceholder.typicode.com/albums");
+  })
+  .then(albums => {
+    albumsLoad(albums);
+    return xhr("GET", "https://jsonplaceholder.typicode.com/posts");
+  })
+  .then(posts => {
+    postsLoad(posts);
+    return xhr("GET", "https://jsonplaceholder.typicode.com/comments");
+  })
+  .then(comments => {
+    commentsLoad(comments);
+  });
